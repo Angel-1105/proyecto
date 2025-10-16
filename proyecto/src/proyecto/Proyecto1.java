e13a4eeec0881de6c216ad0e7426c90eb12aaf82
Ángel Molina <amolred1105@g.educaand.es>
2025-10-16 12:30:59 +0200

    version 1.2

--- proyecto/src/proyecto/Proyecto1.java
+++ proyecto/src/proyecto/Proyecto1.java
@@ -1,5 +1,11 @@
 package proyecto;
 
 public class Proyecto1 {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("AAAAAAAAAAAAAAAAA");
+		System.out.println("jdfwefh");
+	}
+
 
 }
